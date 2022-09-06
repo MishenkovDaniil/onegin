376aa881cd76698c9297676f3425e4fd57f696e2
MishenkovDaniil <mishenkovdaniil@mail.ru>
2022-09-06 20:48:38 +0300

    [2.0] add read function, array of string pointers and sort strings

--- str_main.cpp
+++ str_main.cpp
@@ -1,38 +1,160 @@
-#include "str.h"
+#include <sys\stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include <strings.h>
+#include <assert.h>
+#include <ctype.h>
+
+const char Onegin[] = "Eugine Onegin.txt";
+
+int read_file (FILE *src_file, char *pstr, int *nlines, int *maxlen);
+int read_by_str (char *pstr, char **strings);
+int compare_str (const void *str1, const void *str2);
+void print_lex_sort (char **strings, const int nlines, FILE *file);
+
+struct stat buf = {};
 
 int main ()
 {
-    /*
-    printf ("%d\n", my_strlen ("bubu"));
-    char str1[5] = "foo";
-    char str2[5] = "pupu";
-    printf ("%d\n", my_strlen (str1));
-    printf ("%d\n", my_strlen (str2));
-    */
-    //char dest[20] = "pururi";
-    //printf ("%s\n", my_strcat (dest, "foo"));
+    setlocale (LC_ALL, "Rus");
 
-    //int a = printf ("%c", '\0');
-    //printf ("%d", a);
 
-    //my_puts ("dfosi");
-    //my_puts ("0");
-    //my_puts (" ");
+    if ((stat (Onegin, &buf)) == -1)
+    {
+        printf ("ERROR: stat");
+    }
+    else
+    {
+    FILE *src_file = fopen (Onegin, "r");
+    assert(src_file);
 
-    //printf ("%s", my_strncpy (dest, "fjdpofsdjksfoopa", 5));
+    int nlines = 0;
+    int maxlen = 0;
 
-    //printf ("%s", my_strncat (dest, "fsjjiousfsjs", 4));
-    //char str[20] = {0};
+    char *pstr = (char *)malloc (1 * buf.st_size);
 
-    //printf ("%s", fgets (str, 20, stdin));
-    //printf ("%s", str);
+    read_file (src_file, pstr, &nlines, &maxlen);
 
-    //printf ("%s", my_strdup (str));
-    //char line[100] = {0};
-    //int len = my_getline(line, 100, stdin);
-    printf ("%d\n", len);
-    printf ("%s\n", line);
-    //printf ("d%\n%s", getline(line, 100), line);
+    char *strings[nlines];
+
+    read_by_str (pstr, strings);
+
+    qsort (strings, nlines, sizeof (strings[0]), &compare_str);
+
+    for (int i = 0; i < nlines; i++)
+    {
+        printf ("%s\n", strings[i]);
+    }
+
+    FILE *dst_file = fopen ("new onegin.txt", "w");
+
+    print_lex_sort (strings, nlines, dst_file);
+
+    free (pstr);
+    }
 
     return 0;
+}
+
+int read_file (FILE *src_file, char *pstr, int *nlines, int *maxlen)
+{
+    int c = 0;
+    int i = 0;
+    int len = 0;
+
+    while ((c = fgetc (src_file)) != EOF)
+    {
+        *(pstr + i) = c;
+
+        len++;
+        i++;
+
+        if (c == '\n')
+        {
+            (*nlines)++;
+
+            if (len > *maxlen)
+            {
+                *maxlen = len;
+            }
+
+            len = 0;
+        }
+    }
+    *(pstr + i) = '\0';
+
+    return i;
+}
+
+int read_by_str (char *pstr, char **strings)
+{
+    assert (pstr);
+    assert (strings);
+
+    int line = 0;
+    int actual_symbol = 0;
+    int start = 0;
+
+    while (pstr[start] != '\0')
+    {
+        for (actual_symbol = start; pstr[actual_symbol] != '\n'; actual_symbol++)
+        {
+            if (pstr[actual_symbol] == '\0')
+            {
+                strings[line] = &pstr[start];
+
+                return 0;
+            }
+        }
+        pstr[actual_symbol] = '\0';
+
+        strings[line] = &pstr[start];
+
+        line++;
+
+        start = ++actual_symbol;
+
+    }
+
+    return 0;
+}
+
+int compare_str (const void *str1, const void *str2)
+{
+    assert (str1);
+    assert (str2);
+
+    const char *arr1 = *(const char **) str1;
+    const char *arr2 = *(const char **) str2;
+
+    assert (arr1);
+    assert (arr2);
+
+    while ((!(isalnum (*arr1))) && (*arr1 != '\0'))//
+    {
+        arr1++;
+    }
+    while ((!(isalnum (*arr2))) && (*arr2 != '\0'))//
+    {
+        arr2++;
+    }
+
+    while ((*arr1++ == *arr2++) && (*arr1 != '\0') && (*arr2 != '\0'));
+        ;
+
+    return *arr1 - *arr2;
+}
+
+void print_lex_sort (char **strings, const int nlines, FILE *file)
+{
+    assert (strings);
+    assert (file);
+
+    for (int i = 0; i < nlines; i++)
+    {
+        fputs (strings[i], file);
+        printf ("%s\n", strings [i]);
+        fputc ('\n', file);
+    }
 }
